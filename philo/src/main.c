3ab7f4bb7e971b3d9ef5c636a1e0c2716070cdd6
noureh10 <noureh.it@outlook.com>
2024-03-26 18:24:45 +0100

    7 - Modifying main function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,16 +6,25 @@
 /*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:24:50 by nechaara          #+#    #+#             */
-/*   Updated: 2024/03/25 18:19:36 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/03/26 18:21:26 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+
+static int	manage_philosophers(int ac, char **av)
+{
+	if (!invalid_arg(ac, av))
+	{
+		printf("byebye");
+		return (1);
+	}
+}
 
 int main(int ac, char **av)
 {
 	if (!((ac == 5) ^ (ac == 6)))
 		return (error_handler(WRONG_EXEC));
 	else
-		printf("place holder");
+		return (manage_philosophers(ac, av));
 }
