03bd4867a52f1c768cc1533331448caf8252880c
noureh10 <noureh.it@outlook.com>
2024-04-28 22:28:24 +0200

    42 - Changing filename for output functions

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,13 +6,12 @@
 /*   By: nechaara <nechaara.student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 00:37:29 by nechaara          #+#    #+#             */
-/*   Updated: 2024/04/24 18:40:22 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/04/28 19:00:09 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-// Finish function to declutter philo_eat function
 static	t_philo *get_right_philo(t_philo_list *philo_list, t_philo *philo)
 {
 	static t_philo_list	*head_of_philo_list;
@@ -46,7 +45,7 @@ static	int try_lock_ressource(pthread_mutex_t *mutex, int *remaining_time)
 			return (1);
 		res = pthread_mutex_lock(mutex);
 	}
-	*remaining_time = starting_time - current_time;
+	remaining_time = starting_time - current_time;
 	return (0);
 }
 
@@ -56,15 +55,15 @@ int	philo_eat(t_philo_list *philo_list, t_philo *philo, t_table *table, int rema
 	t_philo				*right_philo;
 	int					current_time;
 	int					time_at_end;
-	int					time;
+	int					*time;
 
 	if (!philo_list || !philo || !table)
 		return (NULL_PARAMS);
 	current_time = remaining_ms;
 	right_philo = get_right_philo(philo_list, philo);
-	if (try_lock_ressource(&right_philo->fork->fork, &time))
+	if (try_lock_ressource(&right_philo->fork->fork, time))
 		return (philo->is_dead = true, table->finished_sim = true, PHILO_DEAD);
-	if (try_lock_ressource(&philo->fork->fork, &time))
+	if (try_lock_ressource(&philo->fork->fork, time))
 		return (philo->is_dead = true, table->finished_sim = true, PHILO_DEAD);
 	philo->state_of_philo = EAT;
 	ft_usleep(table->time_to_eat);
@@ -88,16 +87,32 @@ int	philo_sleep(t_table *table, t_philo *philo)
 	return (time_at_end - current_time);
 }
 
-typedef struct t_point
+void	check_state_of_philos(int timeinms, t_philo_list *philo_list)
 {
-	int x;
-	int y;
-	int z;
-}	t_point;
+	t_state state;
+	size_t	philo_id;
 
-int test2(t_point p);
-
-int test(void)
-{
-	test2((t_point){3,2,1});
+	if (!philo_list)
+		return ;
+	while (philo_list)
+	{
+		state = philo_list->content->state_of_philo;
+		philo_id = philo_list->content->philosophers_id;
+		if (state == EAT )
+			print_status(timeinms, philo_id, IS_EATING);
+		else if (state == THINK)
+			print_status(timeinms, philo_id, IS_THINKING);
+		else if (state == SLEEP)
+			print_status(timeinms, philo_id, IS_SLEEPING);
+		else if (state == DEAD)
+		{
+			print_status(timeinms, philo_id, IS_DEAD);
+			break;
+		}
+		if (state != TRANSIT_STATE)
+			philo_list->content->already_printed = true;
+		else
+			philo_list->content->already_printed = false;
+		philo_list = philo_list->next;
+	}
 }
